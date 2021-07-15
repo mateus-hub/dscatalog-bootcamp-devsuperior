dca9c7da205c7b5727ade72fac0db67a09500656
Mateus Silva <theusskt2018@gmail.com>
2021-07-14 22:24:01 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <h1 className="bg-primary">Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>  
   );
 }
 
