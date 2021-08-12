8173d7f28f974227f50116212926e2ac85abee7a
Mateus Silva <theusskt2018@gmail.com>
2021-08-11 23:50:31 -0300

    Organizing utils

---
+++ frontweb/src/util/auth.ts
@@ -0,0 +1,47 @@
+import jwtDecode from 'jwt-decode';
+import { getAuthData } from './storage';
+
+export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+export type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
+
+// Funções auxiliares para testar os roles "perfis de usuários (manipular tokens, roles etc...)"
+export const getTokenData = (): TokenData | undefined => {
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData();
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+export const hasAnyRoles = (roles: Role[]): boolean => {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  /* Forma alternativa "Função de alta ordem"
+    if(tokenData !== undefined) {
+      return roles.some(role => tokenData.authorities.includes(role));
+    }*/
+
+  if (tokenData !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+      if (tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
