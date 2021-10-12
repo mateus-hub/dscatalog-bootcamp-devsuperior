fa0d170e81b64438c259f65f4c25dbfdbcad5521
mateus-hub <theusskt2018@gmail.com>
2021-10-11 23:03:16 -0300

    ButtonIcon test

---
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from "..";
+
+
+test('ButtonIcon should render button with given text', () => {
+
+    // ARRANGE
+    const text = "Fazer login";
+
+    //ACT 
+    render(
+        <ButtonIcon text={text} />
+    );
+
+
+    //ASSERT
+    expect(screen.getByText(text)).toBeInTheDocument();
+});
