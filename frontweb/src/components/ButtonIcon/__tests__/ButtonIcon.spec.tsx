8f210ad244007cc4cf815b43c6243bac42e5beb8
mateus-hub <theusskt2018@gmail.com>
2021-10-13 12:55:37 -0300

    data-testid demonstration

--- frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -15,4 +15,5 @@ test('ButtonIcon should render button with given text', () => {
 
     //ASSERT
     expect(screen.getByText(text)).toBeInTheDocument();
+    expect(screen.getByTestId("arrow")).toBeInTheDocument();
 });
