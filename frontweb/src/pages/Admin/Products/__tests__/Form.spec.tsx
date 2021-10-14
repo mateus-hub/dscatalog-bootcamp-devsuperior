65de688b47b8b23035ea0b072f349acd6e80e18d
mateus-hub <theusskt2018@gmail.com>
2021-10-14 15:46:53 -0300

    test should clear validation messages when filling out the form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -24,6 +24,39 @@ describe('Product form create tests', () => {
         })
     });
 
+    test('should show toast and redirect when submit form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+
+        const nameInput = screen.getByTestId("name");
+        const priceInput = screen.getByTestId("price");
+        const imgUrlInput = screen.getByTestId("imgUrl");
+        const descriptionInput = screen.getByTestId("description");
+        const categoriesInput = screen.getByLabelText("Categorias");
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i})
+
+        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+        userEvent.type(nameInput, 'Computador');
+        userEvent.type(priceInput, '5000.12');
+        userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+        userEvent.type(descriptionInput, 'Computador muito bom');
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElement).toBeInTheDocument();
+        });
+   
+        expect(history.location.pathname).toEqual('/admin/products');
+    });
+
     test('should show 5 validation messages when just clicking submit', async () => {
 
         render(
@@ -40,8 +73,41 @@ describe('Product form create tests', () => {
             const messages = screen.getAllByText('Campo obrigatório');
             expect(messages).toHaveLength(5);
         });
+    });
 
-        
+    test('should clear validation messages when filling out the form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+        );
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i})
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const messages = screen.getAllByText('Campo obrigatório');
+            expect(messages).toHaveLength(5);
+        });
+
+        const nameInput = screen.getByTestId("name");
+        const priceInput = screen.getByTestId("price");
+        const imgUrlInput = screen.getByTestId("imgUrl");
+        const descriptionInput = screen.getByTestId("description");
+        const categoriesInput = screen.getByLabelText("Categorias");
+
+        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+        userEvent.type(nameInput, 'Computador');
+        userEvent.type(priceInput, '5000.12');
+        userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+        userEvent.type(descriptionInput, 'Computador muito bom');
+
+        await waitFor(() => {
+            const messages = screen.queryAllByText('Campo obrigatório');
+            expect(messages).toHaveLength(0);
+        });
     });
 });
 
