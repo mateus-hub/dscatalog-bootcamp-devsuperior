5e21a34c397c613263918e1e93ab4870180a58a0
mateus-hub <theusskt2018@gmail.com>
2021-10-14 17:24:32 -0300

    test update should show toast and redirect when submit form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import Form from "../Form";
 import history from 'util/history';
 import { Router, useParams } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
-import { server } from "./fixtures";
+import { productResponse, server } from "./fixtures";
 import selectEvent from "react-select-event";
 import { ToastContainer } from "react-toastify";
 
@@ -108,6 +108,60 @@ describe('Product form create tests', () => {
             const messages = screen.queryAllByText('Campo obrigatório');
             expect(messages).toHaveLength(0);
         });
+    });
+});
+
+describe('Product form update tests', () => {
+
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: '2'
+        })
+    });
+
+    test('should show toast and redirect when submit form correctly', async () => {
+
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+
+        await waitFor(() => {
+            const nameInput = screen.getByTestId("name");
+            const priceInput = screen.getByTestId("price");
+            const imgUrlInput = screen.getByTestId("imgUrl");
+            const descriptionInput = screen.getByTestId("description");
+
+            const formElement = screen.getByTestId("form");
+            
+            expect(nameInput).toHaveValue(productResponse.name);
+            expect(priceInput).toHaveValue(String(productResponse.price));
+            expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+            expect(descriptionInput).toHaveValue(productResponse.description);
+
+            const ids = productResponse.categories.map(x =>String(x.id));
+            expect(formElement).toHaveFormValues({categories: ids});
+        });
+        
+
+           const submitButton = screen.getByRole('button', { name: /salvar/i})
+
+        // await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+        // userEvent.type(nameInput, 'Computador');
+        // userEvent.type(priceInput, '5000.12');
+        // userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+        // userEvent.type(descriptionInput, 'Computador muito bom');
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso');
+            expect(toastElement).toBeInTheDocument();
+        });
+   
+        // expect(history.location.pathname).toEqual('/admin/products');
     });
 });
 
